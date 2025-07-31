66b956e7925ac6b7246561c8292abee1c251d082
OwnerDEPER <syssolutions.pekerele@gmail.com>
2025-07-31 09:45:18 -0500

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,28 +4,28 @@ const chalk = require('chalk');
 
 // Contact details
 global.sessionid = process.env.SESSION_ID || '';
-global.ytname = process.env.YT_NAME || "YT: @EliteProTechs";
-global.socialm = process.env.SOCIAL_M || "GitHub: EliteProTech";
-global.location = process.env.LOCATION || "Nigeria, Port Harcourt";
+global.ytname = process.env.YT_NAME || "YT: 𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓";
+global.socialm = process.env.SOCIAL_M || "GitHub: 𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓";
+global.location = process.env.LOCATION || "America/Lima";
 
 // Creator details
-global.ownernumber = process.env.OWNER_NUMBER || '2348109263390';
-global.ownername = process.env.OWNER_NAME || 'ElitePro';
-global.botname = process.env.BOT_NAME || 'ELITE-PRO-V1';
+global.ownernumber = process.env.OWNER_NUMBER || '51941847465';
+global.ownername = process.env.OWNER_NAME || '𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓';
+global.botname = process.env.BOT_NAME || '𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓';
 
 // Default settings 
-global.prefix = process.env.PREFIX || '.';
+global.prefix = process.env.PREFIX || '#';
 // Settings: true=enable false=disable
 global.autoRecording = process.env.AUTO_RECORDING === 'true';
 global.autoTyping = process.env.AUTO_TYPING === 'true';
 global.autorecordtype = process.env.AUTO_RECORD_TYPE === 'true';
 global.autoread = process.env.AUTO_READ === 'true';
-global.autobio = process.env.AUTO_BIO !== 'false'; // default true
+global.autobio = process.env.AUTO_BIO !== 'true'; // default true
 global.anti92 = process.env.ANTI_92 === 'true';
 global.autoswview = process.env.AUTO_SW_VIEW !== 'false'; // default true
 global.welcome = process.env.WELCOME !== 'false'; // default true
-global.autoreact = process.env.AUTO_REACT === 'true';
-global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'true';
+global.autoreact = process.env.AUTO_REACT === 'false';
+global.autolikestatus = process.env.AUTO_LIKE_STATUS === 'false';
 
 // Thumbnail profile picture
 global.elitepropp = process.env.ELITE_PRO_PP || 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg';
@@ -34,10 +34,10 @@ global.themeemoji = process.env.THEME_EMOJI || '👨‍💻';
 
 
 // Sticker details
-global.packname = process.env.PACKNAME || 'Sticker By';
-global.author = process.env.AUTHOR || 'ELITEPRO\n\nContact: +2348109263390';
+global.packname = process.env.PACKNAME || '𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓';
+global.author = process.env.AUTHOR || '𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓\n\nContact: +51941847465';
 // Default settings 2
-global.wm = process.env.WM || "Youtube @EliteProTechs";
+global.wm = process.env.WM || "Youtube : 𝚂𝚈𝚂𝚂𝙾𝙻𝚄𝚃𝙸𝙾𝙽𝚂✓";
 global.link = process.env.LINK || 'https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g';
 
 // Reply messages
